2359b32dbbb1276e49cea73451f983783e6eeebf
Ian Band <ian.r.band@gmail.com>
2020-07-23 17:51:11 -0700

    timeline loads

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 // REACT //
-import React, {useReducer, useEffect} from 'react';
+import React, {useReducer, useEffect, useState} from 'react';
 
 // HOOKS //
 import DispatchContext from './hooks/dispContext.js';
 import useCombinedReducer from './hooks/combineReducers.js';
 
-import Timeline from './components/Timeline.js';
 
 
 // COMPONENTS //
@@ -14,13 +13,35 @@ import Timeline from './components/Timeline.js';
 
 const App = () => {
 
-    useEffect(() => {
-        console.log('awake');
-    }, []);
+    const [loaded, setLoaded] = useState(false); // change to hook?
 
-    return <Timeline
-        options={'idk'}
-    />;
+    useEffect(() => {
+        // load timeline data from api
+
+
+    });
+
+    const loadTL = () => {
+        const script = document.createElement('script');
+        const timeline_json = { events: [] }
+        script.innerHTML=`var timeline = new TL.Timeline('timeline', ${JSON.stringify(timeline_json)}, {theme_color: '#288EC3', ga_property_id : 'UA-26278437-20'});`;
+
+        // TODO
+        let startID = false, id = 0;
+        if(startID) {
+            script.innerHTML+=`; timeline.goToId("${id}");`
+        }
+
+        document.body.appendChild(script);
+    }
+
+
+
+    return loaded ?
+        <div/> //not sure yet, TL container?
+        :
+        <button onClick={loadTL}>load tl</button>
+        ;
 }
 
 export default App;
--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,20 +2,18 @@ import React, {useEffect, useRef, useState} from 'react';
 
 const Timeline = ({options}) => {
 
+    
+
     const scriptRef = useRef(null);
 
+
+
     const onTLLoad = () => {
-        // this might work
-        console.log('TL after load: ', TL);
+        
 
     }; 
 
-    return <script 
-                ref={scriptRef} 
-                onLoad={onTLLoad} 
-                src={'ewuiorhfgierfgh_timeline.js'} // specified with CopyWebpackPlugin
-                async
-            />;
+    return <script ref={scriptRef} />;
 }
 
 export default Timeline;
