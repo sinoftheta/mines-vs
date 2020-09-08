60665cce8124a4d1f7fe7f4e3342e851ce71a97c
Band <bandi@oregonstate.edu>
2020-09-07 17:18:39 -0700

    added peer debuginfo

--- src/logic/multiGame.js
+++ src/logic/multiGame.js
@@ -55,13 +55,16 @@ export default class MultiGame{
 
         // Register with the peer server
         if(process.env.VUE_APP_USE_PUBLIC_PEERJS){
-            this.peer = new Peer({});
+            this.peer = new Peer({
+                debug: 3,
+            });
         }
         else{
             this.peer = new Peer({
                 host: process.env.VUE_APP_PEER_SERVER,
                 port: process.env.VUE_APP_PORT || 8082,
-                path: '/'
+                path: '/',
+                debug: 3
             });
         }
 
@@ -144,7 +147,7 @@ export default class MultiGame{
                 setTimeout(this.startGame, adjustedCountTime);
                 break;
             case leftClick:
-                this.opponentLeftClick(data.x, data.y);
+                this.opponentLeftClick(data.x, data.y, data.ts);
 
         }
     }
@@ -168,10 +171,18 @@ export default class MultiGame{
         });
         console.log(`you scored: ${points}, your total: ${this.userPoints += points}`);
     }
-    opponentLeftClick(x,y){
+    opponentLeftClick(x,y, ts){
         // check for overlap. i.e. if need to rollback
         // send overlap command if needed
 
+
+        // create a dictionary with x,y as the key, and the timestamp & owner & point value of click as the value
+        let hub = {
+            _1_2: {ts: 123, owner: 'host', points: 12}
+        }
+        // if we get an opponent click with an earlier timestamp, we reclaim all the tiles and the points that were earned with that click
+        // if the opponent click has a later timestamp, ignore it. your click will be sent to the opponent and they will sync
+        // store list of all moves in list, search list from end to be efficent?
         //update board
         const points = this.boardState.revealPoints(x,y, !this.host);
         this.board.drawAll();
